c155e6d86edca7e16868aacbd995d7d0bc2fd421
KangHeeWon <kkhw1202@gmail.com>
2021-07-25 23:36:40 +0900

    Add cavas size, additional comments

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = document.getElementsByClassName('canvas')[0].offsetWidth;
+canvas.height = document.getElementsByClassName('canvas')[0].offsetHeight;
+
 ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
@@ -14,15 +17,18 @@ function startPainting() {
     painting = true;
 }
 
+// 마우스는 계속 움직이고 있다
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
+        // 그리고 있지 않을때
+        ctx.beginPath(); // 계속해서 path, 즉 선을 그린다. (경로)
+        ctx.moveTo(x, y); // 해당 좌표를 따라다닌다 (선의 시작)
     } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+        // 그릴때 (마우스를 움직이는 내내 발생한다)
+        ctx.lineTo(x, y); // 선의 끝 (현재의 sub-path에서 마지막 지점을 특정 좌표로 연결 === 즉, 마우스를 떼지않고 계속 그릴때 연결되어보이는 것을 뜻함, 이전 위치와 연결)
+        ctx.stroke(); // 선을 그린다
     }
 }
 
